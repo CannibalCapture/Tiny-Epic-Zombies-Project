817c14733999a5b71bb4099a89f72d74979085d2
Toby <tobydavisvarnell@gmail.com>
2024-11-07 12:48:36 +0000

    Added an in-game screen which displays the game board. Functionality includes an "Exit Game" button and that's it.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-import sqlite3
-import pygame, pygame_gui
+import pygame, pygame_gui, bcrypt, sqlite3
 from pygame.locals import *
+import pygame_gui.elements.ui_image
 
 def createNewUser(username, password):
     flag = True
+    salt = bcrypt.gensalt()
+    password = hash(password, salt)
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    SQL = "INSERT INTO Users(username, password) VALUES(?,?)"
+    SQL = "INSERT INTO Users(username, password, salt) VALUES(?,?, ?)"
     try:
-        cursor.execute(SQL, (username, password))
+        cursor.execute(SQL, (username, password, salt))
         connection.commit()
     except:
         flag = False
@@ -16,9 +18,19 @@ def createNewUser(username, password):
     connection.close()
     return flag
 
+def hash(data, salt):
+    bytes = data.encode('utf-8')
+    hashedData = bcrypt.hashpw(bytes, salt) 
+    return hashedData
+
+# createNewUser("Toby", "password123")
+
 def loginUser(username, password):
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
+    SQL = "SELECT Salt FROM Users WHERE username = ?"
+    salt = cursor.execute(SQL, (username,)).fetchone()
+    password = hash(password, salt[0])
     SQL = "SELECT ID FROM Users WHERE username = ? and password = ?"
     result = cursor.execute(SQL, (username, password)).fetchone()
     connection.close()
@@ -26,11 +38,11 @@ def loginUser(username, password):
         return result[0]
     else:
         return -1
-    
+
 pygame.init()
 
-WIDTH = 1100
-HEIGHT = 500
+WIDTH = 700
+HEIGHT = 600
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
@@ -41,7 +53,14 @@ loginButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 150), 
 
 
 mainMenu = pygame_gui.UIManager((WIDTH, HEIGHT))
-welcomeLabel = pygame_gui.elements.UILabel(pygame.Rect((WIDTH/2, HEIGHT/2), (200, 100)),text="Welcome", manager=mainMenu)
+welcomeLabel = pygame_gui.elements.UILabel(pygame.Rect((WIDTH/2, 50), (200, 100)),text="Welcome", manager=mainMenu)
+logoutButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 150), (100, 25)), text="Logout", manager=mainMenu)
+startGameButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH/2, HEIGHT/2), (100, 25)), text="Start Game", manager=mainMenu)
+
+gameboard = pygame_gui.UIManager((WIDTH, HEIGHT))
+gameboardSurf = pygame.image.load("C:\\Users\\DELL\\Desktop\\School-Note\\Computing\\Tiny-Epic-Zombies\\TinyEpicZombies-Code\\gameboard.png")
+pygame_gui.elements.ui_image.UIImage(relative_rect=pygame.Rect((0, 0), (WIDTH, HEIGHT)), image_surface=gameboardSurf, manager=gameboard)
+exitGameButton = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((20, 20), (100, 25)), text="Exit game", manager=gameboard)
 
 manager = login_page
 
@@ -53,11 +72,17 @@ while run:
         if event.type == QUIT:
             run = False
         elif event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == loginButton:
+            if event.ui_element == loginButton: # calls the login process
                 id = loginUser(usernameInput.text, passwordInput.text)
-                print(id)
                 if id > -1:
                     manager = mainMenu
+            elif event.ui_element == logoutButton: # returns to login screen
+                manager = login_page
+            elif event.ui_element == startGameButton: # sends to game board screen
+                manager = gameboard
+            elif event.ui_element == exitGameButton: # exit game to main menu
+                manager = mainMenu
+            
         manager.process_events(event)
     
     manager.update(time_delta)
@@ -72,6 +97,3 @@ while run:
     pygame.display.update()
 
 pygame.quit()
-
-
-# createNewUser("Toby", "password123")
