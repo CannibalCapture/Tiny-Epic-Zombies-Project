07f7657347b07d8a17ce978cef891f055ec64036
Toby <tobydavisvarnell@gmail.com>
2024-11-25 22:59:19 +0000

    Deckmanager bug fixes.

--- TinyEpicZombies/deckmanager.py
+++ TinyEpicZombies/deckmanager.py
@@ -7,16 +7,17 @@ class DeckManager:
         self.searchDeck = []
         self.createDecks()
 
-    def createDecks(self): # Decks are not currently randomised. 
+    def createDecks(self): # Decks are not currently randomised - random.shuffle(list) - will do it,
+        # but drawing is already randomised. 
         for i in range(6):
             self.supplyDeck.append(MeleeWeapon())
         for i in range(6):
             self.searchDeck.append(MeleeWeapon())
     
     def drawSupply(self):
-        card = randint(0, len(self.supplyDeck))
+        card = randint(0, len(self.supplyDeck)-1)
         return self.supplyDeck.pop(card) # Both returns and removes the drawn card from the list. 
     
     def drawSearch(self):
-        card = randint(0, len(self.searchDeck))
+        card = randint(0, len(self.searchDeck)-1)
         return self.searchDeck.pop(card)
