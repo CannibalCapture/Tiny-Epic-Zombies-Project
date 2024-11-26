3fc54460d4dc6f1bc8a8b4cdf50e523a9889377c
Toby <tobydavisvarnell@gmail.com>
2024-11-26 09:47:11 +0000

    Assets folder has subcategories for types of card. Started to add specific weapons as individual classes.

--- TinyEpicZombies/card.py
+++ TinyEpicZombies/card.py
@@ -1,13 +1,15 @@
 class Card:
-    def __init__(self, player=-1, image="", type=""):
-        self.player = player
-        self.image = image
-        self.type = type
+    def __init__(self, player=-1):
+        pass
 
 class MeleeWeapon(Card):
     def __init__(self):
-        pass
+        self.type = "MELEE WEAPON"
+
+class RangedWeapon(Card):
+    def __init__(self):
+        self.type = "RANGED WEAPON"
 
 class BackpackItem(Card):
     def __init__(self):
-        pass
+        self.type = "BACKPACK ITEM"
---
+++ TinyEpicZombies/weapons.py
@@ -0,0 +1,21 @@
+from .card import Card, MeleeWeapon, RangedWeapon
+from TinyEpicZombies.assets.weapons import *
+from listener import Listener
+
+# Implement events for listeners into a weapons factory class. 
+
+class Crowbar(MeleeWeapon):
+    def __init__(self, image=""):
+        super.__init__()
+        self.image = "crowbar.jpg"
+
+    def crowbarMove(self, player, coords): # Needs to be updated to ensure the store moved to is adjacent. 
+        if coords[1] == 0: # Ensures player is moving to an entrance room. 
+            return True
+        return False
+    
+
+class golfClub(MeleeWeapon, Listener):
+    def __init__(self, image=""):
+        super().__init__()
+        
